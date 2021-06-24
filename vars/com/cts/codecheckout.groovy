0fb47693ee55214ecbdb7a0e3bfbab0b98b584f5
iambasil13 <47686156+iambasil13@users.noreply.github.com>
2021-06-24 13:55:58 +0530

    Update codecheckout.groovy

--- vars/com/cts/codecheckout.groovy
+++ vars/com/cts/codecheckout.groovy
@@ -1,1 +1,8 @@
-
+def call(Map stageParams) {
+ 
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url,credentialsId:'9ce7e4ae-f637-48ac-b85c-2afe3b7d8913']]
+    ])
+  }
