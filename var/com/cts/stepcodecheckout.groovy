d541301e6432834d70699ce1b0b93510345cd6a9
iambasil13 <47686156+iambasil13@users.noreply.github.com>
2021-06-24 12:15:41 +0530

    Delete stepcodecheckout.groovy

--- var/com/cts/stepcodecheckout.groovy
+++
@@ -1,9 +0,0 @@
-import com.cts.*
-def call(Map stageParams) {
- 
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
